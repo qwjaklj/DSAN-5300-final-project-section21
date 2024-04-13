af7858317e8e05e87f2719089199030b546f569f
Yicheng Zhu <yz1232@georgetown.edu>
2024-04-13 14:31:43 -0400

    upload datasets and some codes

---
+++ script/data_cleaning.r
@@ -0,0 +1,11 @@
+library(readr)
+library(dplyr)
+data <- read_csv('GamingStudy_data.csv', locale = locale(encoding = 'ISO-8859-1'))
+age <- read_csv('age.csv')
+data_cleaned <- data %>% select(-highestleague, -Reference, -accept, -League, -streams, -Platform)
+unique_games <- unique(data_cleaned$Game)
+age$Proportion <- as.numeric(sub("%", "", age$Proportion)) / 100
+game_categories <- setNames(seq_along(unique_games), unique_games)
+data_cleaned$Game_Category <- as.integer(factor(data_cleaned$Game, levels = names(game_categories)))
+write_csv(data_cleaned, 'data_cleaned.csv')
+write_csv(age,'age_final.csv')
